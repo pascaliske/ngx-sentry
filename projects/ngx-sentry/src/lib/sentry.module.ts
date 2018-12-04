3c3a434f3a20b7159cb324b58705775984801aba
Pascal Iske <info@pascal-iske.de>
2018-12-04 17:43:22 +0100

    refactor(module): init sentry on app initialization

--- projects/ngx-sentry/src/lib/sentry.module.ts
+++ projects/ngx-sentry/src/lib/sentry.module.ts
@@ -1,7 +1,28 @@
-import { NgModule, ModuleWithProviders, ErrorHandler } from '@angular/core'
+import { NgModule, ModuleWithProviders, ErrorHandler, APP_INITIALIZER } from '@angular/core'
+import { init } from '@sentry/browser'
 import { SentryErrorHandler } from './sentry.handler'
 import { ModuleOptions, OPTIONS } from './options'
 
+/**
+ * Initializer function to setup sentry logging.
+ *
+ * @param - The module options
+ * @returns - A promise for waiting to be resolved
+ */
+export function initialize(options: ModuleOptions): () => Promise<void> {
+    // configure sentry's browser library
+    const promise = async (): Promise<void> => {
+        if (options.enabled) {
+            init(options.sentry)
+        }
+    }
+
+    return promise
+}
+
+/**
+ * Injectable http interceptor for sentry.
+ */
 @NgModule({
     imports: [],
     declarations: [],
@@ -9,8 +30,10 @@ import { ModuleOptions, OPTIONS } from './options'
 })
 export class SentryModule {
     /**
-     * Provides
-     * @param options
+     * Provides all necessary providers for sentry connection.
+     *
+     * @param - The module options
+     * @returns - The module with all providers
      */
     public static forRoot(options: ModuleOptions): ModuleWithProviders {
         return {
@@ -19,6 +42,12 @@ export class SentryModule {
                 {
                     provide: OPTIONS,
                     useValue: options,
+                },
+                {
+                    provide: APP_INITIALIZER,
+                    useFactory: initialize,
+                    deps: [OPTIONS],
+                    multi: true,
                 },
                 {
                     provide: ErrorHandler,
