e6b298ea948a74d3356dc8225f71f3fbd79ba3d7
Pascal Iske <info@pascal-iske.de>
2020-01-13 23:11:24 +0100

    refactor: update demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { SentryService } from '@pascaliske/ngx-sentry'
 
 @Component({
     selector: 'app-root',
@@ -6,7 +7,9 @@ import { Component } from '@angular/core'
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+    public constructor(private sentryService: SentryService) {}
+
     public trigger(): void {
-        throw new Error('DEMO!')
+        this.sentryService.captureException(new Error('DEMO!'))
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component'
             http: {
                 enabled: true,
             },
+            dialog: true,
         }),
     ],
     declarations: [AppComponent],
