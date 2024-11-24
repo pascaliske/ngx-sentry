d09fd93f847a936a5ee81bf86b892cf47a56fe72
Pascal Iske <info@pascaliske.dev>
2024-11-24 22:53:45 +0100

    feat: upgrade angular to v19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { SentryService } from '@pascaliske/ngx-sentry'
 @Component({
     selector: 'cmp-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
     public constructor(private readonly sentryService: SentryService) {}
---
+++ src/app/app.config.ts
@@ -0,0 +1,20 @@
+import type { ApplicationConfig } from '@angular/core'
+import { importProvidersFrom } from '@angular/core'
+import { SentryModule } from '@pascaliske/ngx-sentry'
+
+export const appConfig: ApplicationConfig = {
+    providers: [
+        importProvidersFrom(
+            SentryModule.forRoot({
+                enabled: true,
+                sentry: {
+                    dsn: 'https://your-sentry-dsn@sentry.io',
+                },
+                http: {
+                    enabled: true,
+                },
+                dialog: true,
+            }),
+        ),
+    ],
+}
